29aee4c4feb2984117226d7a5188b78c25c8ee83
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-09 13:32:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+writeCards(["ken", "brandon", "kev"], "birthday");
+function writeCards(names, event) {
+    let thankyou = [];
+    for (let i = 0; i < names.length; i++) {
+        thankyou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankyou;
+}
+
+function countDown() {
+    let count = 10;
+    while (count>= 0) {
+        console.log(count);
+        count--;
+    }
+}
